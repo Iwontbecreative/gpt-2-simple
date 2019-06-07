5785d7c983548359b4d0f6c6986cdf6f6e0ab8a1
Thibault Fevry <thibault.fevry@benevolent.ai>
2019-06-07 18:49:23 -0400

    Add tasks

--- scripts/dataset_convert.py
+++ scripts/dataset_convert.py
@@ -5,6 +5,10 @@ from enum import Enum
 class Task(Enum):
     MNLI = "mnli"
     RTE = "rte"
+    COLA = "cola"
+    WIC = "wic"
+    WSC = "wsc"
+
 
 MNLI_PATH = "/scratch/tjf324/data/glue_auto_dl/MNLI/train.tsv"
 
@@ -38,14 +42,34 @@ def convert_task(data: pd.DataFrame, task: Task, predict_premise: bool) -> pd.Se
             first_part = data['sentence2']
             second_part = data['sentence1']
         data['sentences'] = first_part + ' | ' + second_part  + " || " + data["label"]
+    elif task == Task.COLA:
+        if predict_premise:
+            print('`predict_premise` does not apply to CoLA')
+        return data.iloc[:, -1] + ' | ' + data.iloc[:, 1]
+    elif task == Task.WIC:
+        if predict_premise:
+            print('`predict_premise` does not apply to WiC (tasks are interchangeable)')
+            print("This does not handle position yet.")
+        first_part = data['sentence1']
+        second_part = data['sentence2']
+        data['sentences'] = first_part + ' | ' + second_part  + " || " + data["label"]
+    elif task == Task.WSC:
+        if predict_premise:
+            print('`predict_premise` does not apply to WSC')
+            print("This does not handle position yet.")
+        data['sentences'] = data["text"] + ' | ' data["label"]
     return data["sentences"]
 
 
 if __name__ == "__main__":
     args = get_args()
     print("Loading...")
-    data = pd.read_csv(args.data_file, sep="\t",
-                       error_bad_lines=False, quoting=3, skiprows=0)
+    data_file = args.data_file
+    if "json" in data_file:
+        data = pd.read_json(data_file)
+    else:
+        data = pd.read_csv(data_file, sep="\t",
+                           error_bad_lines=False, quoting=3, skiprows=0)
     output_series = convert_task(data, args.task, args.predict_premise)
     print("Writing...")
     output_series.to_csv(args.output_file, sep="\t", index=False, header=False)
