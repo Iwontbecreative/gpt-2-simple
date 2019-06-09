ae8f432ddff1ee0a0571fe4b3ae744bb5c45bb41
Thibault Fevry <thibault.fevry@benevolent.ai>
2019-06-08 21:06:14 -0400

    More fixes...

--- scripts/clean_labels.py
+++ scripts/clean_labels.py
@@ -3,7 +3,16 @@ import argparse
 import pandas as pd
 from scripts.shared import Task
 
-LABEL_MAP = {"mnli": ["contradiction", "entailment", "neutral"]}
+LABEL_MAP = {
+    "mnli": ["contradiction", "entailment", "neutral"],
+    "rte": ["entailment", "not_entailment"],
+    "cola": [0, 1],
+    "copa": [0, 1],
+}
+
+TASK_TRANSFORMS = {
+    "mnli": mnli_fix,
+}
 
 
 def get_args(*in_args):
@@ -19,14 +28,20 @@ def get_args(*in_args):
     return args
 
 
+def mnli_fix():
+    pass
+
+
+
 def main():
     args = get_args()
     task = args.task
     fn = args.input_file
-    assert task in LABEL_MAP, f"{task} not supported"
+    assert task.value in LABEL_MAP, f"{task} not supported"
+    authorized_labels = LABEL_MAP[task.value]
     data = pd.read_csv(fn, sep="\t")
     print(f"Initially {len(data)} rows")
-    data = data[data.label.isin(LABEL_MAP)]
+    data = data[data.label.isin(authorized_labels)]
     print(f"After label cleaning: {len(data)}")
     data.to_csv(fn, sep="\t", index=False)
 
